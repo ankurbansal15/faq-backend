f24c691d7db15fb9411e9eee61d1b851754b10d2
ankurbansal15 <ankurbansal153@gmail.com>
2025-02-02 20:30:43 +0530

    FAQ Model Created

---
+++ models/faq.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const faqSchema = new Schema({
+    question: {
+        type: String,
+        required: true
+    },
+    answers: {
+        type: [String],
+        required: true
+    },
+    languageMap: {
+        type: Map,
+        of: new Schema({
+            question: {
+                type: String,
+                required: true
+            },
+            answer: {
+                type: String,
+                required: true
+            }
+        }),
+        required: true
+    }
+},{timestamps: true});
+
+const FAQ = mongoose.model('FAQ', faqSchema);
+
+module.exports = FAQ;
